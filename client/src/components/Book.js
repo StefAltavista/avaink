e2b96e8a30d5a66e50d92e81767d31cbb5af4c35
Stef Altavista <stefanomsc@hotmail.it>
2022-11-10 15:25:51 +0100

    calendar admin usage set

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -1,11 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from "./Calendar";
+import { useSelector } from "react-redux";
+import ModalBook from "./ModalBook";
 export default function Book() {
+    const access = useSelector((state) => state.admin.access);
+    const availabilities = useSelector((state) => state.content.calendar);
+    const [myCalendar, setMyCalendar] = useState(availabilities);
+    const [multipleSelection, setMultipleSelection] = useState();
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    const setAvailable = () => {
+        console.log(multipleSelection);
+        let events = [
+            ...myCalendar,
+            available(multipleSelection.startStr, multipleSelection.endStr),
+        ];
+        setMyCalendar(events);
+    };
+
+    const setUnavailable = () => {
+        console.log(multipleSelection);
+        let events = [
+            ...myCalendar,
+            unavailable(multipleSelection.startStr, multipleSelection.endStr),
+        ];
+        setMyCalendar(events);
+    };
+    useEffect(() => {
+        console.log(myCalendar);
+    }, [myCalendar]);
     return (
         <>
-            {" "}
             <h1>Book an appointment</h1>
-            <Calendar></Calendar>
+            <Calendar
+                multiple={(e) => setMultipleSelection(e)}
+                single={(e) => setSelectedDate(e)}
+                availabilities={myCalendar}
+            />
+            {selectedDate && !access && <ModalBook selected={selectedDate} />}
+            {access && (
+                <div>
+                    <button
+                        onClick={() => {
+                            setAvailable();
+                        }}
+                    >
+                        Set Available
+                    </button>
+                    <button onClick={() => setUnavailable()}>
+                        Set Not Available
+                    </button>
+                    <button>Save</button>
+                </div>
+            )}
         </>
     );
 }
+
+const available = (start, end) => {
+    return {
+        id: "Available",
+        start,
+        end,
+        backgroundColor: "LightGreen",
+        display: "background",
+        selectable: true,
+    };
+};
+
+const unavailable = (start, end) => {
+    return {
+        id: "Not Available",
+        start,
+        end,
+        backgroundColor: "Red",
+        display: "background",
+        selectable: false,
+    };
+};
--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,85 +1,21 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
-// import { Calendar } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import { useSelector } from "react-redux";
 
-import ModalBook from "./ModalBook";
-
-export default function Calendar() {
-    const access = useSelector((state) => state.admin.access);
-    const availabilities = useSelector((state) => state.content.calendar);
-    const [myCalendar, setMyCalendar] = useState(availabilities);
-
-    const [multipleSelection, setMultipleSelection] = useState();
-    const [selectedDate, setSelectedDate] = useState(null);
-
-    const cal = useRef();
-    const available = (start, end) => {
-        return {
-            id: "Available",
-            start,
-            end,
-            backgroundColor: "LightGreen",
-            display: "background",
-            selectable: true,
-        };
-    };
-
-    const unavailable = (start, end) => {
-        return {
-            id: "Not Available",
-            start,
-            end,
-            backgroundColor: "Red",
-            display: "background",
-            selectable: false,
-        };
-    };
-
-    const setAvailable = () => {
-        let event = available(multipleSelection.start, multipleSelection.end);
-        console.log(event);
-        setMyCalendar([...myCalendar, event]);
-        console.log("myC", myCalendar);
-        console.log("api", cal.current.getApi());
-    };
-
-    // let Calendario = new Calendar(cal, {
-    //     plugins: [dayGridPlugin, interactionPlugin],
-    //     events: myCalendar,
-    //     selectable: true,
-    //     dateClick: (e) => {
-    //         setSelectedDate(e);
-    //         console.log(myCalendar);
-    //     },
-    //     select: (e) => setMultipleSelection(e),
-    // });
+export default function Calendar({ availabilities, single, multiple }) {
+    useEffect(() => {
+        console.log("change", availabilities);
+    }, [availabilities]);
     return (
         <div id="calendar">
             <FullCalendar
-                ref={cal}
                 plugins={[dayGridPlugin, interactionPlugin]}
-                events={myCalendar}
+                events={availabilities}
                 selectable={true}
-                dateClick={(e) => {
-                    setSelectedDate(e);
-                    console.log(myCalendar);
-                }}
-                select={(e) => setMultipleSelection(e)}
+                dateClick={(e) => single(e)}
+                select={(e) => multiple(e)}
             ></FullCalendar>
-
-            {selectedDate && !access && (
-                <ModalBook selected={selectedDate}></ModalBook>
-            )}
-            {access && (
-                <div>
-                    <button onClick={setAvailable}>Set Available</button>
-                    <button>Set Not Available</button>
-                    <button>Save</button>
-                </div>
-            )}
         </div>
     );
 }
